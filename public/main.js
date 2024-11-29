262298ec434f32a3f92aa541ccb565d53e336890
Adrián Maza <adri_mv1@hotmail.com>
2024-11-29 13:53:37 +0100

    code cleaned

--- public/main.js
+++ public/main.js
@@ -1,7 +1,1 @@
 "use strict";
-// tsc main.ts -w
-// tsc --init
-// tsc -w
-console.log("Hola a todos!");
-console.log("Hot reload con watch");
-console.log("tsconfig añadido");
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-// tsc main.ts -w
-
-// tsc --init
-// tsc -w
-
-console.log("Hola a todos!");
-console.log("Hot reload con watch");
-console.log("tsconfig añadido");
-

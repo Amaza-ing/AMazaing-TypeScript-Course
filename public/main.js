a74d6fe27b327a8b6ba7db592495f5cb7bac1400
Adrián Maza <adri_mv1@hotmail.com>
2024-12-02 17:38:03 +0100

    04 advanced types

--- public/main.js
+++ public/main.js
@@ -1,13 +1,24 @@
 "use strict";
 // Tipos de datos:
+// Union
 let id = 123456;
 id = "abcde";
 let userId = "abc123";
+let myArr = [10, 20, 30];
+myArr = [40, 50, "str1", "str2", true, false];
+const loading = "loading";
+const error = "error";
+let state = "loading";
 const esdla = {
     title: "El Señor de los anillos",
     duration: 200,
     hasOscars: true,
 };
-let myArr = [10, 20, 30];
-myArr = [40, 50, "str1", "str2", true, false];
 console.log(id, esdla, myArr);
+const harryPotter = {
+    title: "Harry Potter",
+    duration: 120,
+    hasOscars: true,
+    pages: 400
+};
+let a;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 // Tipos de datos:
 
+// Union
+
 let id: number | string = 123456;
 id = "abcde";
 
 type id = number | string;
+
 let userId: id = "abc123";
+
+let myArr: (number | string | boolean)[] = [10, 20, 30];
+myArr = [40, 50, "str1", "str2", true, false];
+
+
+// Literal
+
+type stateLoading = "loading";
+type stateError = "error";
+
+const loading: stateLoading = "loading";
+const error: stateError = "error";
+
+type state = stateLoading | stateError;
+
+let state: state = "loading";
 
 type movie = {
   title: string;
@@ -18,7 +37,26 @@ const esdla: movie = {
   hasOscars: true,
 };
 
-let myArr: (number | string | boolean)[] = [10, 20, 30];
-myArr = [40, 50, "str1", "str2", true, false];
-
 console.log(id, esdla, myArr);
+
+
+// Intersection
+
+type book = {
+  title: string;
+  pages: number;
+}
+
+type bookAdaptation = movie & book;
+
+const harryPotter: bookAdaptation = {
+  title: "Harry Potter",
+  duration: 120,
+  hasOscars: true,
+  pages: 400
+}
+
+
+type numStr = number & string;
+
+let a: numStr;

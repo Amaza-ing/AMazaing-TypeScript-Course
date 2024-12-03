0cf2dc3686ec025e31f4b339e5b3636b5af9de9b
Adrián Maza <adri_mv1@hotmail.com>
2024-12-03 15:57:29 +0100

    code cleaned

--- public/main.js
+++ public/main.js
@@ -1,15 +1,2 @@
 "use strict";
-// Genéricos
-// function getFirstElement(array: number[]) {
-//   return array[0];
-// }
-function getFirstElement(array) {
-    return array[0];
-}
-const numArr = [10, 20, 30];
-const firstNum = getFirstElement(numArr);
-const strArr = ["a", "b", "c"];
-const firstStr = getFirstElement(strArr);
-console.log(firstNum, firstStr);
-const username = document.querySelector("#username");
-console.log(username === null || username === void 0 ? void 0 : username.placeholder);
+// Clases
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,2 @@
-// Genéricos
-
-// function getFirstElement(array: number[]) {
-//   return array[0];
-// }
-
-function getFirstElement<T>(array: T[]) {
-  return array[0];
-}
-
-const numArr = [10, 20, 30];
-const firstNum = getFirstElement(numArr);
-
-const strArr = ["a", "b", "c"];
-const firstStr = getFirstElement<string>(strArr);
-
-console.log(firstNum, firstStr);
-
-
-const username = document.querySelector<HTMLInputElement>("#username");
-console.log(username?.placeholder);
-
+// Clases
 

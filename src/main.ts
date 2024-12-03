5a6d9c4d21d12f15c038e1ce49f9cfac7a370fb4
Adrián Maza <adri_mv1@hotmail.com>
2024-12-03 16:07:41 +0100

    classes

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,23 @@
 // Clases
 
+class Movie {
+  title: string;
+  duration: number;
+  hasOscars: boolean;
+
+  constructor(title: string, duration: number, hasOscars: boolean) {
+    this.title = title;
+    this.duration = duration;
+    this.hasOscars = hasOscars;
+  }
+
+  getInfo() {
+    return `Title: ${this.title} - Duration: ${this.duration} - Has Oscars: ${this.hasOscars}`;
+  }
+}
+
+const movie1 = new Movie("El Señor de los Anillos", 300, true);
+const movie2 = new Movie("Harry Potter", 120, true);
+
+console.log(movie1, movie2);
+

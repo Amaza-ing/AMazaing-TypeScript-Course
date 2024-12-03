15b5af12e9067880523d9d8ba667a25f221b6fab
Adrián Maza <adri_mv1@hotmail.com>
2024-12-03 16:33:53 +0100

    inheritance

--- public/main.js
+++ public/main.js
@@ -10,8 +10,24 @@ class Movie {
         return `Title: ${this.title} - Duration: ${this.duration} - Has Oscars: ${this.hasOscars}`;
     }
 }
+class HorrorMovie extends Movie {
+    constructor(title, duration, hasOscars, hasJumpScares) {
+        super(title, duration, hasOscars);
+        this.hasJumpScares = hasJumpScares;
+    }
+    displayAlert() {
+        console.log("This movie is really scary");
+    }
+    getInfo() {
+        return "Some movie info";
+    }
+}
 const movie1 = new Movie("El Señor de los Anillos", 300, true);
 const movie2 = new Movie("Harry Potter", 120, true);
 movie1.title = "Star Wars";
 console.log(movie1, movie2);
 console.log(movie1.getInfo());
+const scream = new HorrorMovie("Scream", 90, false, true);
+console.log(scream);
+scream.displayAlert();
+console.log(scream.getInfo());
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,25 @@ class Movie {
   }
 }
 
+class HorrorMovie extends Movie {
+  constructor(
+    title: string,
+    duration: number,
+    hasOscars: boolean,
+    public hasJumpScares: boolean
+  ) {
+    super(title, duration, hasOscars);
+  }
+
+  displayAlert() {
+    console.log("This movie is really scary");    
+  }
+
+  getInfo() {
+    return "Some movie info";
+  }
+}
+
 const movie1 = new Movie("El Señor de los Anillos", 300, true);
 const movie2 = new Movie("Harry Potter", 120, true);
 
@@ -19,3 +38,9 @@ movie1.title = "Star Wars";
 
 console.log(movie1, movie2);
 console.log(movie1.getInfo());
+
+const scream = new HorrorMovie("Scream", 90, false, true);
+
+console.log(scream);
+scream.displayAlert()
+console.log(scream.getInfo());

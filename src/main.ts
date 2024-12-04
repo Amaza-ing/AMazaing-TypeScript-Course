d4a43bc9094567d2befa792fddb8d7b106d5073b
Adrián Maza <adri_mv1@hotmail.com>
2024-12-04 13:55:20 +0100

    namespaces

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,21 @@
-// Módulos
+// Namespaces
 
-import { HorrorMovie } from "./classes/HorrorMovie.js";
-import { Movie } from "./classes/Movie.js";
-import { Director } from "./Models/Director.js";
+namespace Common {
+  export interface User {
+    id: number;
+    username: string;
+  }
 
-const director1: Director<string> = {
-  name: "Peter Jackson",
-  age: 60,
-  data: "He is Amazing"
+  export interface Error {
+    id: number;
+    isCritical: boolean;
+    message: string;
+  }
 }
 
-// console.log(director1);
+const user: Common.User = {
+  id: 1234,
+  username: "Adrián",
+};
 
-const movie1 = new Movie("El Señor de los Anillos", 300, true, director1);
-const movie2 = new Movie("Harry Potter", 120, true, director1);
-
-// movie1.title = "Star Wars";
-
-console.log(movie1, movie2);
-movie1.play();
-// console.log(movie1.getInfo());
-
-const scream = new HorrorMovie("Scream", 90, false, director1, true);
-
-// console.log(scream);
-// scream.displayAlert()
-// console.log(scream.getInfo());
+console.log(user);

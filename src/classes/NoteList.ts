71804b5a8050543102b94c1b0ebb25fce4f5dae1
Adrián Maza <adri_mv1@hotmail.com>
2024-12-05 17:08:16 +0100

    notes app 2

---
+++ src/classes/NoteList.ts
@@ -0,0 +1,24 @@
+import { Note } from "./Note";
+
+export class NoteList {
+  static createNoteCards(notes: Note[]) {
+    const noteList = document.querySelector(".note-list") as HTMLUListElement;
+
+    notes.forEach((note) => {
+      const li = document.createElement("li");
+      const input = document.createElement("input");
+      const checkbox = document.createElement("input");
+
+      input.classList.add("title");
+      input.value = note.getTitle();
+
+      checkbox.type = "checkbox";
+
+      li.classList.add("note-card");
+      li.append(input);
+      li.append(checkbox);
+
+      noteList.append(li);
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,7 @@
 // Notes App
 
 import { Note } from "./classes/Note";
-
-// npm install -g typescript
-//
-// tsconfig.json:
-//   "module": "ESNext",
-//   "rootDir": "./src",
-//   "outDir": "./public",
-//   "include": ["src"]
-//
-// npm init
-// npm i webpack webpack-cli ts-loader typescript webpack-dev-server -D
-// webpack.config.js
-// package.json
-//
-// npm i jest babel-jest @babel/core @babel/preset-env @babel/preset-typescript @types/jest -D
-// babel.config.js
-// package.json
+import { NoteList } from "./classes/NoteList";
 
 const createNote = (title: string) => {
   const id = Date.now().toString(36) + Math.random().toString(36).slice(2);
@@ -33,27 +17,6 @@ const notes: Note[] = [];
 notes.push(note1);
 notes.push(note2);
 
-const createNoteCards = (notes: Note[]) => {
-  const noteList = document.querySelector(".note-list") as HTMLUListElement;
-
-  notes.forEach((note) => {
-    const li = document.createElement("li");
-    const input = document.createElement("input");
-    const checkbox = document.createElement("input");
-
-    input.classList.add("title");
-    input.value = note.getTitle();
-
-    checkbox.type = "checkbox";
-
-    li.classList.add("note-card");
-    li.append(input);
-    li.append(checkbox);
-
-    noteList.append(li);
-  });
-};
-
 const btn = document.querySelector(".btn") as HTMLButtonElement;
 
-btn.addEventListener("click", () => createNoteCards(notes));
+btn.addEventListener("click", () => NoteList.createNoteCards(notes));

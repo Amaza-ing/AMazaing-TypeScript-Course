9489bb37153c6fc9ed865fa777bdf98d2d0c0e77
Adrián Maza <adri_mv1@hotmail.com>
2024-12-05 12:39:45 +0100

    base code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-// Webpack dev server
-
-// npm i webpack-dev-server -D
+// HTML & Eventos
 
 import { HorrorMovie } from "./classes/HorrorMovie";
 import { Movie } from "./classes/Movie";
@@ -12,19 +10,7 @@ const director1: Director<string> = {
   data: "He is Amazing"
 }
 
-// console.log(director1);
-
 const movie1 = new Movie("El Señor de los Anillos", 300, true, director1);
 const movie2 = new Movie("Harry Potter", 120, true, director1);
-
-// movie1.title = "Star Wars";
-
-console.log(movie1, movie2);
-movie1.play();
-// console.log(movie1.getInfo());
-
 const scream = new HorrorMovie("Scream", 90, false, director1, true);
 
-// console.log(scream);
-// scream.displayAlert()
-// console.log(scream.getInfo());

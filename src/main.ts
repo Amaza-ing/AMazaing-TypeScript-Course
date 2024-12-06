5d85ea6d0e8a6017f020f4e0084a58f1cd907089
Adrián Maza <adri_mv1@hotmail.com>
2024-12-06 13:11:36 +0100

    notes app 3

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,16 @@ notes.push(note2);
 const btn = document.querySelector(".btn") as HTMLButtonElement;
 
 btn.addEventListener("click", () => NoteList.createNoteCards(notes));
+
+const noteForm = document.querySelector(".note-form") as HTMLFormElement;
+
+noteForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // console.log(e);
+  const form = e.target as HTMLFormElement;
+  const input = form.elements[0] as HTMLInputElement;
+
+  const newNote = createNote(input.value);
+  notes.push(newNote);
+  NoteList.createNoteCards(notes);
+});
